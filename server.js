429d7c9f935641af421466889b1fff529ecaffb1
lytruongsinh <diepduong607@gmail.com>
2024-10-27 17:45:26 +0700

    changes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+require('dotenv').config()
 const port = process.env.PORT;
 const path = require('path');
 app.use('/public', express.static(path.join(__dirname, '/public')))
